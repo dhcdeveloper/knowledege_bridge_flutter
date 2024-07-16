656585ba15fef48e626b92ee456bf67bb7478348
MrLv <boqian.lv@dhc.com.cn>
2024-07-16 16:57:58 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Demo１',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
